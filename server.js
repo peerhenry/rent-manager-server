6e511b88d204b2535b5d7bafb6356980b36e77eb
PeerHenry <peerhenry@gmail.com>
2018-11-12 21:08:27 +0100

    graphql

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+var graphqlHTTP = require('express-graphql');
+var graphql = require('graphql');
+var { buildSchema } = require('graphql');
 
 // setting up node server loosely following:
 // https://medium.freecodecamp.org/building-a-simple-node-js-api-in-under-30-minutes-a07ea9e390d2
@@ -9,7 +11,7 @@ const app = express();
 
 const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // https://www.valuepenguin.com/auto-insurance/car-make-model
 // make is the brand
@@ -43,10 +45,28 @@ const vehicles = [
   }
 ]
 
-app.get('/vehicle-list', (req, res) => {
-  res.send(vehicles)
-});
+var schema = buildSchema(`
+type Query {
+  hello: String
+}
+`);
+var root = {
+  hello: () => {
+    return 'hello world!';
+  }
+};
 
+// GraphQL endpoint?
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+  pretty: true
+}));
+// REST get
+/*app.get('/vehicle-list', (req, res) => {
+  res.send(vehicles)
+});*/
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
